d5466509400dd7c9791ac895901fe95cc9d71670
nikitaIvanzolochenko <nikitaudin2007Isip233aIvanzolochenko@mail.ru>
2025-09-30 17:03:43 +0900

    commonUpdate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./components/Header";
-
+import localFont from "next/font/local";
 
 export const metadata: Metadata = {
   title: "Мой сайт",
   description: "Описание моего сайта",
 };
+
+//export const myFont = localFont({
+//src: "./my-font.woff2",
+//});
 
 export default function RootLayout({
   children,
@@ -14,7 +18,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ru">
+    <html
+      lang="ru" //className={myFont.className}
+    >
       <body>
         <Header />
         {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+import { ThemeProvider, Button } from "@material-tailwind/react";
 export default function Home() {
   return (
     <main>
@@ -13,9 +15,56 @@ export default function Home() {
           <button className="w-36.25 h-11.25 bg-[#FFCF01] font-bold rounded-2xl cursor-pointer shadow-[0_7px_36px_0_rgba(255,207,1,0.2)]">
             Get started
           </button>
+          <Image
+            src="/More.svg"
+            alt="Больше"
+            width={20.5}
+            height={21}
+            className="object-contain cursor-pointer hidden"
+          />
         </div>
       </div>
       <div></div>
+      <Image
+        src="/Stars.svg"
+        alt=""
+        width={38}
+        height={38.51}
+        className="mt-20 ml-18.5 absolute"
+      />
+      <h1 className="text-white font-bold mt-[106px]! ml-4 mr-4 w-full text-4xl text-center">
+        Ready <p></p> for every project
+      </h1>
+      <div className="w-full flex flex-wrap gap-3 justify-center">
+        <button className="bg-[#182632] px-[12px] py-[14px] rounded-[45px] text-[14px] text-[#FFB4D3] w-fit">
+          Investment DAO
+        </button>
+        <button className="bg-[#182632] px-[12px] py-[14px] rounded-[45px] text-[14px] text-[#FFC24C] w-fit">
+          Startup DAO
+        </button>
+        <button className="bg-[#182632] px-[12px] py-[14px] rounded-[45px] text-[14px] text-[#D387F7] w-fit">
+          Service DAO
+        </button>
+        <button className="bg-[#182632] px-[12px] py-[14px] rounded-[45px] text-[14px] text-[#77E6FF] w-fit">
+          Community DAO
+        </button>
+        <button className="bg-[#182632] px-[12px] py-[14px] rounded-[45px] text-[14px] text-[#23C9DE] w-fit">
+          Impact DAO
+        </button>
+        <button className="bg-[#182632] px-[12px] py-[14px] rounded-[45px] text-[14px] text-[#FFB4D3] w-fit">
+          DeFi DAO
+        </button>
+        <button className="bg-[#182632] px-[12px] py-[14px] rounded-[45px] flex text-[14px] text-[#707F8D] w-fit">
+          <Image
+            src="/Star.svg"
+            alt=""
+            width={10}
+            height={10}
+            className="mr-[3px]"
+          />
+          More
+        </button>
+      </div>
     </main>
   );
 }
